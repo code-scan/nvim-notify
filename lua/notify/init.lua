fd97fc99b198bc6ef77f965aea460fc1382bcefe
c <c@box>
2022-12-16 01:42:36 +0800

    disable-lsp-message

--- lua/notify/init.lua
+++ lua/notify/init.lua
@@ -83,9 +83,9 @@ function notify.notify(message, level, opts)
   if not global_instance then
     notify.setup()
   end
-  -- if string.find(message,"Format") then
-  --   return false
-  -- end
+  if string.find(message,"Format") then
+    return false
+  end
 
   return global_instance.notify(message, level, opts)
 end
